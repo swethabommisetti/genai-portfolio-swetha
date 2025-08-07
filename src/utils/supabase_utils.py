bc21e81d526f7422343ff50c395b5245e12f5fcc
Swetha Bommisetti <swethasetti.kandukuri@gmail.com>
2025-08-07 10:35:33 -0500

    making it work again - secrets.toml fixed

--- src/utils/supabase_utils.py
+++ src/utils/supabase_utils.py
@@ -3,6 +3,9 @@ import streamlit as st
 
 @st.cache_resource
 def get_supabase_client():
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
+    #url = st.secrets["SUPABASE_URL"]
+    #key = st.secrets["SUPABASE_KEY"]
+    url = st.secrets["supabase"]["url"]
+    key = st.secrets["supabase"]["SUPABASE_SERVICE_KEY"]
+    
     return create_client(url, key)
