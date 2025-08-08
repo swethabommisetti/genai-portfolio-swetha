1c66e498469a3c3acb4273ded2e95ed19b5179a9
Swetha Bommisetti <swethasetti.kandukuri@gmail.com>
2025-08-08 13:12:14 -0500

    try7

--- src/utils/visitor_service.py
+++ src/utils/visitor_service.py
@@ -1,11 +1,12 @@
 from supabase import create_client
 import os
+from utils.supabase_utils import get_supabase_client
 
 # Setup client
-supabase = create_client(
-    os.getenv("SUPABASE_URL"),
-    os.getenv("SUPABASE_SERVICE_ROLE_KEY")
-)
+#supabase = create_client(
+#    os.getenv("SUPABASE_URL"),
+#    os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+#)
 
 def fetch_or_insert_visitor_id(email: str) -> str:
     """Insert email into visitors (if needed), return visitor_id"""
@@ -13,10 +14,10 @@ def fetch_or_insert_visitor_id(email: str) -> str:
         return None
 
     # Step 1: Try fetch
-    result = supabase.table("visitors").select("id").eq("email", email).limit(1).execute()
+    result = get_supabase_client.table("visitors").select("id").eq("email", email).limit(1).execute()
     if result.data:
         return result.data[0]["id"]
 
     # Step 2: Insert
-    insert = supabase.table("visitors").insert({"email": email}).execute()
+    insert = get_supabase_client.table("visitors").insert({"email": email}).execute()
     return insert.data[0]["id"]
